cec2d0227cbb5dd80fa142cadf9831dc0b0b632d
trofimovhh <trofimov.hh@gmail.com>
2023-10-28 00:32:26 +1300

    feat: added Checkout page and tests

--- CourseEvaluation/Tests/BuyProductTests.cs
+++ CourseEvaluation/Tests/BuyProductTests.cs
@@ -6,7 +6,7 @@ namespace CourseEvaluation.Tests;
 
 public class BuyProductTests : WebDriverInit
 {
-	[Test(Description = "E2E test, that checks possibility to login with correct data and make a successful purchase.")]
+	[Test(Description = "E2E test, that checks possibility to login with correct data and make a purchase.")]
 	public void BuyProduct()
 	{
 		// Arrange
@@ -54,72 +54,6 @@ public class BuyProductTests : WebDriverInit
 
 		// Assert
 		Assert.That(confirmationPage.GetGratitudeNotification(), Is.EqualTo("Thank you for your order!"));
-	}
-
-	[Test(Description =
-		"Test confirms impossibility to make a purchase with an empty First Name field in the Checkout page.")]
-	public void BuyProductWithEmptyFirstName()
-	{
-		// Arrange
-		LoginPage loginPage = new LoginPage(driver);
-		InventoryPage inventoryPage = new InventoryPage(driver);
-		CartPage cart = new CartPage(driver);
-		CheckoutPage checkoutPage = new CheckoutPage(driver);
-		loginPage.Login(userNameLogin, userPassword);
-		
-		// Act
-		inventoryPage.ClickAddCartSauceLabsBackpackButton();
-		inventoryPage.ClickCartButton();
-		cart.ClickCheckoutButton();
-		checkoutPage.FillFields(emptyString, lastName, postalCode);
-		checkoutPage.ClickContinueButton();
-
-		// Assert
-		Assert.That(checkoutPage.GetErrorNotification(), Is.EqualTo(checkoutPage.GetErrorFirstNameNotification()));
-	}
-
-	[Test(Description =
-		"Test confirms impossibility to make a purchase with an empty 'Last Name' field in the Checkout page.")]
-	public void BuyProductWithIncorrectLastName()
-	{
-		// Arrange
-		LoginPage loginPage = new LoginPage(driver);
-		InventoryPage inventoryPage = new InventoryPage(driver);
-		CartPage cart = new CartPage(driver);
-		CheckoutPage checkoutPage = new CheckoutPage(driver);
-
-		// Act
-		loginPage.Login(userNameLogin, userPassword);
-		inventoryPage.ClickAddCartSauceLabsBackpackButton();
-		inventoryPage.ClickCartButton();
-		cart.ClickCheckoutButton();
-		checkoutPage.FillFields(firstName, emptyString, postalCode);
-		checkoutPage.ClickContinueButton();
-
-		// Assert
-		Assert.That(checkoutPage.GetErrorNotification(), Is.EqualTo(checkoutPage.GetErrorLastNameNotification()));
-	}
-
-	[Test(Description =
-		"Test confirms impossibility to make a purchase with an empty 'Zip/Postal Code' field in the Checkout page.")]
-	public void BuyProductWithIncorrectPostalCode()
-	{
-		// Arrange
-		LoginPage loginPage = new LoginPage(driver);
-		InventoryPage inventoryPage = new InventoryPage(driver);
-		CartPage cart = new CartPage(driver);
-		CheckoutPage checkoutPage = new CheckoutPage(driver);
-
-		// Act
-		loginPage.Login(userNameLogin, userPassword);
-		inventoryPage.ClickAddCartSauceLabsBackpackButton();
-		inventoryPage.ClickCartButton();
-		cart.ClickCheckoutButton();
-		checkoutPage.FillFields(firstName, lastName, emptyString);
-		checkoutPage.ClickContinueButton();
-
-		// Assert
-		Assert.That(checkoutPage.GetErrorNotification(), Is.EqualTo(checkoutPage.GetErrorPostalCodeNotification()));
 	}
 
 	[Test(Description =
---
+++ CourseEvaluation/Tests/CheckoutTests.cs
@@ -0,0 +1,74 @@
+﻿using CourseEvaluation.Pages;
+using NUnit.Framework;
+using static CourseEvaluation.Data.UserData;
+
+namespace CourseEvaluation.Tests;
+
+public class CheckoutTests : WebDriverInit
+{
+	[Test(Description =
+		"Test confirms impossibility to make a purchase with an empty First Name field in the Checkout page.")]
+	public void CheckoutWithEmptyFirstName()
+	{
+		// Arrange
+		LoginPage loginPage = new LoginPage(driver);
+		InventoryPage inventoryPage = new InventoryPage(driver);
+		CartPage cart = new CartPage(driver);
+		CheckoutPage checkoutPage = new CheckoutPage(driver);
+		loginPage.Login(userNameLogin, userPassword);
+		
+		// Act
+		inventoryPage.ClickAddCartSauceLabsBackpackButton();
+		inventoryPage.ClickCartButton();
+		cart.ClickCheckoutButton();
+		checkoutPage.FillFields(emptyString, lastName, postalCode);
+		checkoutPage.ClickContinueButton();
+
+		// Assert
+		Assert.That(checkoutPage.GetErrorNotification(), Is.EqualTo(checkoutPage.GetErrorFirstNameNotification()));
+	}
+
+	[Test(Description =
+		"Test confirms impossibility to make a purchase with an empty 'Last Name' field in the Checkout page.")]
+	public void CheckoutWithEmptyLastName()
+	{
+		// Arrange
+		LoginPage loginPage = new LoginPage(driver);
+		InventoryPage inventoryPage = new InventoryPage(driver);
+		CartPage cart = new CartPage(driver);
+		CheckoutPage checkoutPage = new CheckoutPage(driver);
+
+		// Act
+		loginPage.Login(userNameLogin, userPassword);
+		inventoryPage.ClickAddCartSauceLabsBackpackButton();
+		inventoryPage.ClickCartButton();
+		cart.ClickCheckoutButton();
+		checkoutPage.FillFields(firstName, emptyString, postalCode);
+		checkoutPage.ClickContinueButton();
+
+		// Assert
+		Assert.That(checkoutPage.GetErrorNotification(), Is.EqualTo(checkoutPage.GetErrorLastNameNotification()));
+	}
+
+	[Test(Description =
+		"Test confirms impossibility to make a purchase with an empty 'Zip/Postal Code' field in the Checkout page.")]
+	public void CheckoutWithEmptyPostalCode()
+	{
+		// Arrange
+		LoginPage loginPage = new LoginPage(driver);
+		InventoryPage inventoryPage = new InventoryPage(driver);
+		CartPage cart = new CartPage(driver);
+		CheckoutPage checkoutPage = new CheckoutPage(driver);
+
+		// Act
+		loginPage.Login(userNameLogin, userPassword);
+		inventoryPage.ClickAddCartSauceLabsBackpackButton();
+		inventoryPage.ClickCartButton();
+		cart.ClickCheckoutButton();
+		checkoutPage.FillFields(firstName, lastName, emptyString);
+		checkoutPage.ClickContinueButton();
+
+		// Assert
+		Assert.That(checkoutPage.GetErrorNotification(), Is.EqualTo(checkoutPage.GetErrorPostalCodeNotification()));
+	}
+}
