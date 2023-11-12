8c91c75521163e7f313af865f1165739d473384e
trofimovhh <trofimov.hh@gmail.com>
2023-11-12 17:44:35 +1300

    fix: CartTests refactoring

--- CourseEvaluation/Tests/CartTests.cs
+++ CourseEvaluation/Tests/CartTests.cs
@@ -15,14 +15,17 @@ public class CartTests : TestBase
 		var inventoryPage = new InventoryPage(driver);
 		var cart = new CartPage(driver);
 		loginPage.Login(UserData.userNameLogin, UserData.userPassword);
+		report.Log(Status.Info, "User successfully logs into the system");
 
 		//Act
 		inventoryPage.ClickAddCartSauceLabsBackpackButton();
+		report.Log(Status.Info, "User adds a backpack to the cart");
 		inventoryPage.ClickCartButton();
-		report.Log(Status.Info, "LOL");
+		report.Log(Status.Info, "User navigates to the cart page");
 
 		// Assert
 		Assert.That(cart.ListOfItems(), Is.EqualTo(1));
+		report.Log(Status.Info, "User added an item; the number of items in the cart is 1");
 	}
 
 	[Test(Description =
@@ -34,33 +37,45 @@ public class CartTests : TestBase
 		var inventoryPage = new InventoryPage(driver);
 		var cart = new CartPage(driver);
 		loginPage.Login(UserData.userNameLogin, UserData.userPassword);
+		report.Log(Status.Info, "User successfully logs into the system");
 		inventoryPage.ClickAddCartSauceLabsBackpackButton();
+		report.Log(Status.Info, "User adds a backpack to the cart");
 		inventoryPage.ClickCartButton();
+		report.Log(Status.Info, "User navigates to the cart page");
 
 		//Act
 		cart.RemoveOneItemFromCart();
+		report.Log(Status.Info, "User removes one item from the cart");
 
 		// Assert
 		Assert.That(cart.ListOfItems(), Is.EqualTo(0));
+		report.Log(Status.Info, "User removed an item from the cart; the number of items in the cart is now 0");
 	}
 
 
 	[Test(Description = "Test confirms possibility to add all product items to cart and then remove one item.")]
-	public void AddAllItemsToCartAndThenDeleteOneItem()
+	public void AddAllItemsToCartAndThenRemoveOneItem()
 	{
 		// Arrange
 		var loginPage = new LoginPage(driver);
 		var inventoryPage = new InventoryPage(driver);
 		var cart = new CartPage(driver);
 		loginPage.Login(UserData.userNameLogin, UserData.userPassword);
+		report.Log(Status.Info, "User successfully logs into the system");
 		inventoryPage.AddAllItemsOfProductsToCart();
+		report.Log(Status.Info, "User adds all items to the cart");
 		inventoryPage.ClickCartButton();
+		report.Log(Status.Info, "User navigates to the cart page");
+		report.Log(Status.Info, $"The number of items in the cart is {cart.ListOfItems()}");
 
 		//Act
 		cart.RemoveOneItemFromCart();
+		report.Log(Status.Info, "User removes one item from the cart");
 
 		// Assert
 		Assert.That(cart.ListOfItems(), Is.EqualTo(5));
+		report.Log(Status.Info,
+			$"The item has been successfully removed from the cart; the number of items in the cart is {cart.ListOfItems()}");
 	}
 
 	[Test(Description =
@@ -74,6 +89,7 @@ public class CartTests : TestBase
 		loginPage.Login(UserData.userNameLogin, UserData.userPassword);
 		inventoryPage.ClickAddCartSauceLabsBackpackButton();
 		inventoryPage.ClickCartButton();
+		report.Log(Status.Info, "User navigates to the cart page");
 
 		//Act
 		cart.ClickContinueShoppingButton();
